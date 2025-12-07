04228258f7c785c976021110bae21bb1813d031a
ZHanNp <ketsaraporn.ru@kkumail.com>
2025-12-08 00:15:32 +0700

    Add BubbleSort.java implementation

---
+++ Project/BubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter numbers (Bubble Sort): ");
+        String[] input = sc.nextLine().trim().split("\\s+");
+        int[] numbers = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
+        }
+
+        bubbleSort(numbers);
+
+        System.out.print("Sorted: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
